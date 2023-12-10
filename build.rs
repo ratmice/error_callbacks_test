45c05d7fc91acfd3cf8bb885a4cd2c5c6b1d9c6e
matt rice <ratmice@gmail.com>
2023-12-10 15:51:22 -0800

    Collect spans about productions (unused currently)

--- build.rs
+++ build.rs
@@ -1,12 +1,13 @@
+use cfgrammar::yacc::ast::{self, GrammarAST};
 use cfgrammar::yacc::YaccGrammar;
 use cfgrammar::yacc::YaccGrammarError;
-use cfgrammar::yacc::ast::GrammarAST;
 use cfgrammar::yacc::YaccKind;
+use cfgrammar::Span;
 use lrlex::{CTLexerBuilder, DefaultLexerTypes, LexBuildError};
 use lrpar::LexerTypes;
 use lrtable::{statetable::Conflicts, StateGraph, StateTable};
+use num_traits;
 use std::error::Error;
-
 const LEX_FILENAME: &'static str = "erroneous.l";
 const YACC_FILENAME: &'static str = "erroneous.y";
 
@@ -32,25 +33,60 @@ fn grammar_error(errs: Vec<YaccGrammarError>) -> Box<dyn Error> {
     .into()
 }
 
-fn on_unexpected_conflicts(
+fn on_unexpected_conflicts<StorageT>(
     ast: &GrammarAST,
-    grm: &YaccGrammar,
-    _sgraph: &StateGraph<<DefaultLexerTypes as LexerTypes>::StorageT>,
-    stable: &StateTable<<DefaultLexerTypes as LexerTypes>::StorageT>,
-    _conflicts: &Conflicts<<DefaultLexerTypes as LexerTypes>::StorageT>,
-) -> Box<dyn Error> {
+    grm: &YaccGrammar<StorageT>,
+    _sgraph: &StateGraph<StorageT>,
+    stable: &StateTable<StorageT>,
+    _conflicts: &Conflicts<StorageT>,
+) -> Box<dyn Error>
+where
+    usize: num_traits::AsPrimitive<StorageT>,
+    StorageT: std::hash::Hash
+        + std::cmp::Eq
+        + std::marker::Copy
+        + 'static
+        + num_traits::PrimInt
+        + num_traits::Unsigned
+        + std::fmt::Debug,
+{
+    let prods = &ast.prods;
     let mut out = String::new();
+    // I'm not sure yet what of this information is going to be helpful yet.
+    // But here is i believe all of or a good amount of the span information related
+    // to conflicts, their rules, productions the spans of those and their names.
+    //
+    // We'll need to figure out what we actually need
     if let Some(c) = stable.conflicts() {
         for (r1_prod_idx, r2_prod_idx, _st_idx) in c.rr_conflicts() {
-            out.push_str(
-                format!("Reduce/reduce: {:?} {:?}\n", r1_prod_idx.0, r2_prod_idx.0).as_str(),
-            );
+            if usize::from(*r1_prod_idx) < prods.len() {
+                let prod = &prods[usize::from(*r1_prod_idx)];
+                let prod_spans = prod.symbols.iter().map(|sym| match sym {
+                    ast::Symbol::Rule(_, span) => span,
+                    ast::Symbol::Token(_, span) => span,
+                });
+            }
+            let r1_rule = grm.prod_to_rule(*r1_prod_idx);
+            let r2_rule = grm.prod_to_rule(*r2_prod_idx);
+            let r1_span = grm.rule_name_span(r1_rule);
+            let r2_span = grm.rule_name_span(r2_rule);
+
+            out.push_str(format!("Reduce/reduce: {:?} {:?}\n", r1_prod_idx, r2_prod_idx).as_str());
         }
         for (s_tok_idx, r_prod_idx, _st_idx) in c.sr_conflicts() {
             let r_rule_idx = grm.prod_to_rule(*r_prod_idx);
             let span2 = grm.token_span(*s_tok_idx);
             let shift_name = grm.token_name(*s_tok_idx).unwrap();
             let reduce_name = grm.rule_name_str(r_rule_idx);
+            if usize::from(*r_prod_idx) < prods.len() {
+                let prod = &prods[usize::from(*r_prod_idx)];
+                let prod_spans = prod.symbols.iter().map(|sym| match sym {
+                    ast::Symbol::Rule(_, span) => span,
+                    ast::Symbol::Token(_, span) => span,
+                });
+            }
+            let rule = grm.prod_to_rule(*r_prod_idx);
+            let rule_span = grm.rule_name_span(rule);
             out.push_str(
                 format!(
                     "Shift/Reduce: {:?} Shift: {} Reduce: {}\n",
